0a7f0b1a34b871994dadeeeb7f359768a2102b2a
Haim Kalvo <you@example.com>
2021-08-02 14:13:29 +0300

    connecting post.controllers to s3- first try

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,6 +1,14 @@
-
+const AWS = require('aws-sdk');
 const fs = require('fs').promises; //file system- for reading and writing files to the system (for base 64)
-const Post = require ('../models/post')
+const Post = require ('../models/post');
+const keys = require ('../keys/keys.js');
+
+const s3 = new AWS.S3({
+    bucketName: keys.bucketName,
+    signatureVersion: "v4",
+    region: "eu-west-1" 
+});
+
 
 class PostsController {
 
@@ -20,25 +28,30 @@ class PostsController {
         const fileName = req.file.filename;
         
         try {
-            const imageBase64 = await fs.readFile('public/posts/' + fileName, { /*these two lines will be switched when moving to S3*/
-                encoding: 'base64'
+            const fileContent = await fs.readFile('/public/posts/' + fileName);
+            const params = {
+                Bucket : 'nechavot-style',
+                Key: `${keys.folderPosts}/${fileName}`,
+                Body: fileContent
+              };
+              console.log("create", fileName);
+              const uploadedFile = s3.upload(params, (err, data) => {
+                const post = new Post({
+                    description: req.body.description,
+                    image: fileContent,
+                    size: req.body.size
+                });
+                const newPost = post.save();
+                res.status(201).send(newPost);
             });
 
-            const post = new Post({
-                description: req.body.description,
-                image: imageBase64,
-                size: req.body.size
-            });
-
-            const newPost = await post.save();
-            res.status(201).send(newPost);
+            
         } catch(err) {
             console.log(err);
             res.sendStatus(400);
-
         }
-        
     }
+
 }
 
 module.exports = PostsController;
---
+++ src/keys/keys.js
@@ -0,0 +1,8 @@
+
+const keys = {
+    bucketName: 'nechavot-style',
+    folderAvatars: 'avatar',
+    folderPosts: 'posts'
+}
+
+module.exports = keys;
